edc70be6178e4e34ba59f4864a5ac29587aac232
uigabbb <uigun0421@yonsei.ac.kr>
2026-03-13 05:05:39 +0000

    char

---
+++ lec1/lec1_1st.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a = -3;
+    printf("a %%d: %d\n", a);
+    printf("a %%i: %i\n", a);
+    printf("a %%c: %c\n", a + 32);
+    printf("a %%f: %f\n", (float)a);
+    printf("-----\n");
+    return 0;
+}
---
+++ lec1/lec1_1st_2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    char strc[5] = "abcdefghijklmnopqrstuvwxyz";
+    printf("strc %%c: %c\n", strc);
+    printf("strc %%d: %d\n", strc);
+    printf("strc %%s: %s\n", strc);
+    return 0;
+}
